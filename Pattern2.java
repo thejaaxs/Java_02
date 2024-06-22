b3454e278b285726be8cf85d1fc6dd81470299b9
Thejas <thejasganiga150503@gmail.com>
2024-06-22 23:39:02 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,17 @@
+public class Pattern2 {
+    public static void main (String args [])
+{
+    int n = 5;
+for ( int i = 1 ; i<=n ; i++){
+    for(int j = 1 ; j <= n ; j++ ){
+        if(i==1||i==5 || j==5 || j==1){
+            System.out.print("*"+" ");
+        }
+        else{
+            System.out.print("  ");
+        }
+    }
+    System.out.println(" ");
+}
+}
+}
